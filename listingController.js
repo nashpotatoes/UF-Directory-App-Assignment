2a3fe93a6947d7c9618b167ed6d26d7d8d91dd36
nashpotatoes <stephaniefrances98@gmail.com>
2017-09-06 22:32:36 -0400

    Add files via upload

--- listingController.js
+++ listingController.js
@@ -1,14 +1,31 @@
-angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
+angular.module('listings').controller('ListingsController', ['$scope', 'Listings',
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
-
-    /* 
-      Implement these functions in the controller to make your application function 
-      as described in the assignment spec. 
+		$scope.name = "";
+		$scope.code="";
+    /*
+      Implement these functions in the controller to make your application function
+      as described in the assignment spec.
+      display listing
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
-  }
-]);
+    $scope.addListings =function(){
+      $scope.listings.push({
+				code: $scope.code,
+				name: $scope.name,
+        address: $scope.address
+			});
+
+      $scope.code = "";
+      $scope.name = "";
+      $scope.address = "";
+    };
+    $scope.showDetails = function(index){
+     $scope.details = index;
+    };
+    $scope.deleteListing = function (index) {
+      var index = $scope.listings.indexOf(index);
+      $scope.listings.splice(index, 1);
+
+		};
+}]);
