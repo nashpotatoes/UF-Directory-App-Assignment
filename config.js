1067a4555bbf4d2ee91ebf473097773b3075eedf
nashpotatoes <stephaniefrances98@gmail.com>
2017-09-06 22:34:17 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  db: {
+    uri: 'mongodb://snash:30326098sn@ds127034.mlab.com:27034/ufintrotose', //place the URI of your mongo database here.
+  }
+};
+
+/* Now go to the JSONtoMongo.js file and include this file as a variable named 'config' with a require() */
